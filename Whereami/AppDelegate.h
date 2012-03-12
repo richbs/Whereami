9a791ae5dc7286383048be197b0a7b11e77fb24d
Richard Barrett-Small <rickbs@gmail.com>
2012-03-12 22:11:31 +0000

    Initial Commit

---
+++ Whereami/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Whereami
+//
+//  Created by Richard Barrett-Small on 12/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
